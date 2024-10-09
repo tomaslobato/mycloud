6597c4142fe62a986a0d46f93ffee35d3de83258
Tomas Lobato <tlobatodev@gmail.com>
2024-10-09 06:39:03 -0300

    responsive editor

--- client/index.tsx
+++ client/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react"
+import React, { useEffect, useState } from "react"
 import { createRoot } from "react-dom/client"
 import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom"
 import Header from "./components/Header"
@@ -18,14 +18,23 @@ const root = createRoot(document.getElementById("app"))
 
 function Layout() {
   const [editorOpen, setEditorOpen] = useState<{open: boolean, id: string} | null>(null)
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const handleResize = () => setWindowWidth(window.innerWidth);
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  const isMobile = windowWidth < 600;
 
   return (
     <React.StrictMode>
       {/* {sidebarOpen && <Sidebar />} */}
-      <div className="panel" style={editorOpen?.open ? {width: "25%"} : {width: "100%"}}>
+      <div className={`panel`} style={{ display: editorOpen?.open && isMobile ? "none" : "block", width: editorOpen?.open ? "25%" : "100%"}} >
         <Header />
         <Explorer setEditorOpen={setEditorOpen}/>
-      </div>
+      </div>    
       {editorOpen?.open ? <Editor editorOpen={editorOpen} setEditorOpen={setEditorOpen}/> : null}
     </React.StrictMode>
   )
